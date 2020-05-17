a12e7ca5e1b7c8169ae5cd4c92eb3cebfb102035
Vinod Mathew <scrumvisualize@gmail.com>
2020-05-17 17:49:24 +1000

    Added the image in aboutus

--- src/App.js
+++ src/App.js
@@ -14,23 +14,6 @@ import Navigation from './components/Navigation';
 
 var ReactDOM = require('react-dom');
 const App = () => {
-
-  // const componentDidMount = () => {
-  //    //Call our fetch function below once the component mounts
-  //   this.callBackendAPI()
-  //   .then(res => this.setState({ data: res.express }))
-  //   .catch(err => console.log(err));
-  // }
-  //   //Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  //   const callBackendAPI = async () => {
-  //   const response = await fetch('/express_backend');
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) {
-  //     throw Error(body.message)
-  //   }
-  //   return body;
-  // }; 
 return (
       <BrowserRouter>
         <div>
